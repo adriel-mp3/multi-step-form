c8f785123325dfa973e8d5046dbccbcafd6c606f
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-17 18:56:17 -0300

    :package: build: config es-lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,34 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unsafe-finally': 'off',
+    'no-unused-vars': 'off',
+    'react/jsx-key': 'off',
   },
-}
+};
