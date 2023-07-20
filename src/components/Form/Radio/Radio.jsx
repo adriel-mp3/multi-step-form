f2ccbd7ee9a6940448f557105898d1b8042348cf
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-20 16:13:39 -0300

    :iphone: feat(src/components/Form): add responsiveness

--- src/components/Form/Radio/Radio.jsx
+++ src/components/Form/Radio/Radio.jsx
@@ -6,18 +6,25 @@ const Radio = ({ label, name, icon, title, price, value, setValue }) => {
   const { period, selectedPlan } = React.useContext(FormContext);
 
   return (
-    <div className={`${styles.wrapper} ${selectedPlan === name ? styles.activePlan : ''}`}>
+    <div
+      className={`${styles.wrapper} ${
+        selectedPlan === name ? styles.activePlan : ""
+      }`}
+    >
       <label htmlFor={name} className="sr-only">
         {label}
       </label>
       <img src={icon} alt="Icon plan" className={styles.img} />
-      <p className={styles.title}>{title}</p>
-      <span className={styles.price}>
-        {period === "yearly" ? `$${Number(price) * 10}/yr` : `$${price}/mo`}
-      </span>
-      {period === "yearly" && (
-        <span className={styles.yearly}>2 months free</span>
-      )}
+      <div className={styles.planWrapper}>
+        <p className={styles.title}>{title}</p>
+        <span className={styles.price}>
+          {period === "yearly" ? `$${Number(price) * 10}/yr` : `$${price}/mo`}
+        </span>
+        {period === "yearly" && (
+          <span className={styles.yearly}>2 months free</span>
+        )}
+      </div>
+
       <input
         type="radio"
         name={name}
