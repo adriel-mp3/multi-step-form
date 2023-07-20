cff3ba06363f5e816165873ba04438d13f898e2b
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-20 11:27:13 -0300

    :sparkles: feat(src/components/Steps): add summary plan form step

--- src/components/Steps/Summary/Summary.jsx
+++ src/components/Steps/Summary/Summary.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
-
+import React from "react";
+import Header from "../../Form/Header/Header";
+import styles from "./Summary.module.css";
+import { Link } from "react-router-dom";
+import GoBack from "../../Form/Button/GoBack";
+import { FormContext } from "../../../context/FormContext";
+import { prices, adds } from "./helper/summary";
 const Summary = () => {
-  return (
-    <div>Summary</div>
-  )
-}
+  const { selectedPlan, period, addOns } = React.useContext(FormContext);
 
-export default Summary
+  function calculateTotal() {
+    const planTotal = prices[period][selectedPlan];
+    const addOnsTotal = addOns
+      .map((addOn) => adds[period][addOn])
+      .reduce((a, b) => a + b, 0);
+    return planTotal + addOnsTotal;
+  }
+  calculateTotal();
+  return (
+    <div className={`formContainer ${styles.wrapper}`}>
+      <div>
+        <Header
+          title="Finishing up"
+          paragraph="Double-check everything looks OK before confirming."
+        />
+        <div className={styles.summary}>
+          <div className={styles.summaryHeader}>
+            <div>
+              <h3>
+                {selectedPlan} ({period})
+              </h3>
+              <Link to="/select-plan">Change</Link>
+            </div>
+            <span>{`$${prices[period][selectedPlan]}/${
+              period === "monthly" ? "mo" : "yr"
+            }`}</span>
+          </div>
+          <div className={styles.summaryBody}>
+            {addOns
+              ? addOns.map((addOn) => (
+                  <p key={addOn} className={styles.addon}>
+                    {addOn}
+                    <span>{`$${adds[period][addOn]}/${
+                      period === "monthly" ? "mo" : "yr"
+                    }`}</span>
+                  </p>
+                ))
+              : null}
+          </div>
+          <p className={styles.total}>
+            Total (per {period === "monthly" ? "month" : "year"})
+            <span>{`+$${calculateTotal()}/${
+              period === "monthly" ? "mo" : "yr"
+            }`}</span>
+          </p>
+        </div>
+      </div>
+      <div className={styles.buttonsWrapper}>
+        <Link to="/select-plan">
+          <GoBack />
+        </Link>
+        <Link to="/thank-you">
+          <button className={styles.button}>Confirm</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
---
+++ src/components/Steps/Summary/helper/summary.js
@@ -0,0 +1,25 @@
+export const prices = {
+  monthly: {
+    arcade: 9,
+    advanced: 12,
+    pro: 15,
+  },
+  yearly: {
+    arcade: 90,
+    advanced: 120,
+    pro: 150,
+  },
+};
+
+export const adds = {
+  monthly: {
+    "Online Service": 1,
+    "Larger Storage": 2,
+    "Customizable Profile": 2,
+  },
+  yearly: {
+    "Online Service": 10,
+    "Larger Storage": 20,
+    "Customizable Profile": 20,
+  },
+};
