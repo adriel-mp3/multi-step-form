ca1e60271105737715ec5b47657456d4c773762c
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-29 18:49:19 -0300

    feat: import RadioGroup component

--- src/components/Steps/SelectPlan/index.tsx
+++ src/components/Steps/SelectPlan/index.tsx
@@ -5,16 +5,13 @@ import { useNavigate } from "react-router-dom";
 import { FormContext } from "@/context/FormContext";
 
 import { Header } from "@/components/Form/Header";
-import { Radio } from "@/components/Form/Radio/index";
-import { ToggleSwitch } from "@/components/Form/ToggleSwitch/index";
 import { NextStep } from "@/components/Button/NextStep/index";
 import { GoBack } from "@/components/Button/GoBack/index";
-
-import arcadeIcon from "@/assets/images/icon-arcade.svg";
-import advancedIcon from "@/assets/images/icon-advanced.svg";
-import proIcon from "@/assets/images/icon-pro.svg";
+import { Switch } from "@/components/Switch";
+import { RadioGroup } from "@/components/Form/RadioGroup";
 
 import styles from "./styles.module.css";
+import { planOptions } from "./helper/plansOptions";
 
 export const SelectPlan = () => {
   const { selectedPlan, setSelectedPlan, period } =
@@ -26,36 +23,10 @@ export const SelectPlan = () => {
       <form>
         <Header
           title="Select your plan"
-          paragraph="You have the option of monthly or yearly billing."
+          paragraph="You have the optiow of monthly or yearly billing."
         />
         <div className={styles.radioWrapper}>
-          <Radio
-            label="Arcade"
-            name="arcade"
-            title="Arcade"
-            price="9"
-            icon={arcadeIcon}
-            setValue={setSelectedPlan}
-            value={selectedPlan}
-          />
-          <Radio
-            label="Advanced"
-            name="advanced"
-            title="Advanced"
-            price="12"
-            icon={advancedIcon}
-            setValue={setSelectedPlan}
-            value={selectedPlan}
-          />
-          <Radio
-            label="Pro"
-            name="pro"
-            title="Pro"
-            price="15"
-            icon={proIcon}
-            setValue={setSelectedPlan}
-            value={selectedPlan}
-          />
+          <RadioGroup options={planOptions} />
         </div>
         <div className={styles.switcherWrapper}>
           <span
@@ -63,7 +34,7 @@ export const SelectPlan = () => {
           >
             Monthly
           </span>
-          <ToggleSwitch />
+          <Switch label="Plans" hideLabel={true} />
           <span className={period === "yearly" ? `${styles.periodActive}` : ""}>
             Yearly
           </span>
